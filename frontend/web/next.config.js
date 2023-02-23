55b9ea067ca1c7784f309a714269d08a6c2f2f2a
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-23 23:14:26 +0900

    コンフィグを修正

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const rewrites = process.env.NODE_ENV === 'development' ? [
-  {
-    source: '/api/:path*',
-    destination: 'http://localhost:8080/api/:path*',
-  },
-] : [];
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return rewrites;
-  },
-};
+}
+
+module.exports = nextConfig
